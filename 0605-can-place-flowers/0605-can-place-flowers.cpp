684cfd82495dd54b72bba572a4324823c62ca7b1
Wei Wang <iamwestonw@gmail.com>
2025-09-05 15:56:35 +0800

    Time: 0 ms (100%), Space: 24.2 MB (6.23%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int slot = 0;
+        int zero = 0;
+        if(flowerbed[0] == 0) {
+            zero++;
+        }
+        for(size_t i = 0; i < flowerbed.size(); i++) {
+            if(flowerbed[i] == 0) {
+                zero++;
+            }
+            else {
+                if(zero != 0) {
+                    slot = slot + ((zero - 1) / 2);
+                    zero = 0;
+                }
+            }
+        }
+        if(zero != 0) {
+            zero++;
+            slot = slot + ((zero - 1) / 2);
+        }
+        return n <= slot;
+    }
+};
