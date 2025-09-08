80dbfe55b879e3255622039093eb90c6ff4bd727
Wei Wang <iamwestonw@gmail.com>
2025-09-08 14:58:21 +0800

    Time: 4 ms (6.22%), Space: 12.7 MB (96.05%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -8,12 +8,17 @@ public:
         for (int i = 0; i < chars.size(); i++) {
             if (chars[i] != curr) {
                 // Input the previous counts
-                if(count > 1) {
+                if(count >= 10) {
                     std::string counts = std::to_string(count);
                     for (int j = 0; j < counts.size(); j++) {
                         chars[matters] = counts[j];
                         matters++;
                     }
+                }
+                if(count > 1 && count < 10) {
+                    char counts = count + '0';
+                    chars[matters] = counts;
+                    matters++;
                 }
                 // For encountering different char
                 curr = chars[i];
