213e51005936debcbb9a5b0f75b7d8c338d5a681
Wei Wang <iamwestonw@gmail.com>
2025-09-08 14:52:25 +0800

    Time: 0 ms (100%), Space: 13.8 MB (18.79%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int count = 0;
+        int matters = 0;
+        char curr = '0';
+
+        for (int i = 0; i < chars.size(); i++) {
+            if (chars[i] != curr) {
+                // Input the previous counts
+                if(count > 1) {
+                    std::string counts = std::to_string(count);
+                    for (int j = 0; j < counts.size(); j++) {
+                        chars[matters] = counts[j];
+                        matters++;
+                    }
+                }
+                // For encountering different char
+                curr = chars[i];
+                count = 1;
+                chars[matters] = curr;
+                matters++;
+            } else {
+                // Start counting how many
+                count++;
+            }
+        }
+        if(count > 1) {
+            string counts = std::to_string(count);
+            for (int j = 0; j < counts.size(); j++) {
+                chars[matters] = counts[j];
+                matters++;
+            }
+        }
+        return matters;
+    }
+};
