a6f0540686842dbce2e762a36f709da750c4ccf3
Wei Wang <iamwestonw@gmail.com>
2025-12-25 03:46:58 -0800

    Time: 0 ms (100%), Space: 35.5 MB (61.13%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == nullptr) {return nullptr;}
+        if (root->val == val) {
+            return root;
+        }
+        TreeNode* l = searchBST(root->left, val);
+        TreeNode* r = searchBST(root->right, val);
+        if (l != nullptr) {
+            return l;
+        } else if (r != nullptr) {
+            return r;
+        } else {
+            return nullptr;
+        }
+    }
+};
