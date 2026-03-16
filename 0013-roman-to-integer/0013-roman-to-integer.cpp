80e49912d25d29d535bb479fdc55f794eea92c1f
Wei Wang <iamwestonw@gmail.com>
2026-03-15 22:25:33 -0700

    Time: 0 ms (100%), Space: 10 MB (93.8%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        char prev = 'A';
+        for (char c : s) {
+            switch (c) {
+                case 'M':
+                    result += 1000;
+                    if (prev == 'C') {
+                        result -= 200;
+                    }
+                    break;
+                case 'D':
+                    result += 500;
+                    if (prev == 'C') {
+                        result -= 200;
+                    }
+                    break;
+                case 'C':
+                    result += 100;
+                    if (prev == 'X') {
+                        result -= 20;
+                    }                    
+                    break;
+                case 'L':
+                    result += 50;
+                    if (prev == 'X') {
+                        result -= 20;
+                    }
+                    break;
+                case 'X':
+                    result += 10;
+                    if (prev == 'I') {
+                        result -= 2;
+                    }
+                    break;
+                case 'V':
+                    result += 5;
+                    if (prev == 'I') {
+                        result -= 2;
+                    }
+                    break;
+                case 'I':
+                    result += 1;
+            }
+            prev = c;
+        }
+        return result;
+    }
+};
